2bf0ab3242bca5bbeb8197dc0c910dceeba9d930
Phil Nash <philnash@gmail.com>
2020-03-30 10:54:41 +1100

    Adds a test nested directory template

---
+++ nested-directory/functions/admin/admin.js
@@ -0,0 +1,4 @@
+exports.handler = function(context, event, callback) {
+  console.log("This is the admin");
+  callback(null, "");
+};
---
+++ nested-directory/functions/hello.js
@@ -0,0 +1,4 @@
+exports.handler = function(context, event, callback) {
+  console.log("Hello!");
+  callback(null, "");
+};
